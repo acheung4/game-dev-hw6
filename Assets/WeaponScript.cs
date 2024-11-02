5aee83b41bef6d6fe99ff4cd061abe24e27fc5d1
acheung4 <acheung4@villanova.edu>
2024-11-01 22:54:53 -0400

    Finish homework

---
+++ Assets/WeaponScript.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float shootRange = 30f;
+    
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
+    }
+
+    private void Shoot() {
+        RaycastHit hit;
+
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, shootRange)) {
+            Debug.Log("I hit an object");
+        }
+    }
+}
